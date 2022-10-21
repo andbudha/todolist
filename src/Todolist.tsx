efc7689e09c748fa321bdd9d633d9cf848413a47
andbudha <andreibartov@gmail.com>
2022-10-21 16:23:22 +0200

    Type Script added to the App&Todolist components

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -1,8 +1,16 @@
 import React from "react";
 
+type TaskType = {
+    id: number
+    title: string
+    isDone: boolean
+}
+
 type PropsType = {
     title: string
+    tasks: Array<TaskType>
 }
+
 export function Todolist(props: PropsType) {
     return (
         <div className="App">
@@ -13,9 +21,9 @@ export function Todolist(props: PropsType) {
                     <button>+</button>
                 </div>
                 <ul>
-                    <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
-                    <li><input type="checkbox" checked={true}/> <span>JS</span></li>
-                    <li><input type="checkbox" checked={false}/> <span>React</span></li>
+                    <li><input type="checkbox" checked={props.tasks[0].isDone}/> <span>{props.tasks[0].title}</span></li>
+                    <li><input type="checkbox" checked={props.tasks[1].isDone}/> <span>{props.tasks[1].title}</span></li>
+                    <li><input type="checkbox" checked={props.tasks[2].isDone}/> <span>{props.tasks[2].title}</span></li>
                 </ul>
                 <div>
                     <button>All</button>
