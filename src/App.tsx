cecd4288e89c002877ed92ad21b2abcf072e9cbd
andbudha <andreibartov@gmail.com>
2022-10-22 12:07:37 +0200

    remove-buttons added

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,23 @@ import {Todolist} from "./Todolist";
 
 function App() {
 
-    const tasks1 = [
+    let tasks = [
         { id: 1, title: "HTML&CSS", isDone: true },
         { id: 2, title: "JS", isDone: true },
-        { id: 3, title: "ReactJS", isDone: false }
-    ]
-    const tasks2 = [
-        { id: 1, title: "Hello world", isDone: true },
-        { id: 2, title: "I am Happy", isDone: false },
-        { id: 3, title: "Yo", isDone: false }
+        { id: 3, title: "ReactJS", isDone: false },
+        { id: 4, title: "ReactNative", isDone: false }
     ]
 
+    const removeTask=(taskID: number)=>{
+        tasks = tasks.filter(element=> element.id !== taskID);
+    }
     return (
         <div className="App">
-         <Todolist title={'What to learn'} tasks={tasks1}/>
-         <Todolist title={'Songs'} tasks={tasks2}/>
+         <Todolist
+             title={'What to learn'}
+             tasks={tasks}
+             removeTask={removeTask}
+         />
         </div>
     );
 }
