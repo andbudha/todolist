0eeaf0661b6f604d0c5d3129445480df0e8e0424
andbudha <andreibartov@gmail.com>
2022-10-22 15:08:45 +0200

    useState hook added

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import {Todolist} from "./Todolist";
 
 function App() {
 
-    let tasks = [
+    let [tasks, setTasks]=useState([
         { id: 1, title: "HTML&CSS", isDone: true },
         { id: 2, title: "JS", isDone: true },
         { id: 3, title: "ReactJS", isDone: false },
         { id: 4, title: "ReactNative", isDone: false }
-    ]
+    ]);
+
 
     const removeTask=(taskID: number)=>{
-        tasks = tasks.filter(element=> element.id !== taskID);
+        setTasks(tasks.filter(element=> element.id !== taskID));
     }
     return (
         <div className="App">
